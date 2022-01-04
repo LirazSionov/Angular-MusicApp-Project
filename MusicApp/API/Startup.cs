6e1075e1239415f0a10ad5f98b4d55ee917bfa88
Liraz Sionov <lirazsionov@gmail.con>
2022-01-05 00:05:42 +0200

    Insert into DB values

--- MusicApp/API/Startup.cs
+++ MusicApp/API/Startup.cs
@@ -11,22 +11,32 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+       
+        private readonly IConfiguration _config;
+        
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
+           
+            
+          
         }
 
-        public IConfiguration Configuration { get; }
-
+        
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options=>
+            options.UseSqlite(_config.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
