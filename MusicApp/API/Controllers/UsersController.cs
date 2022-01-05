27f200ae2a108f33f6e26dbeead31844fccfb21b
Liraz Sionov <lirazsionov@gmail.con>
2022-01-05 02:35:57 +0200

    change to async+HttpGet types

---
+++ MusicApp/API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using System.Linq;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+     public UsersController(DataContext context)
+     {
+            _context = context;
+         
+     }   
+    [HttpGet]
+     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+     {
+         var users=await _context.Users.ToListAsync();
+         return users;
+     }
+
+     [HttpGet("{id}")]
+     public async Task<ActionResult<AppUser>> GetUser(int id)
+     {
+         var user=await _context.Users.FindAsync(id);
+         return user;
+     }
+    }
+}
